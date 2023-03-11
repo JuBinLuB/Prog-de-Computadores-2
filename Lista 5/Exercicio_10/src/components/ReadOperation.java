ee51c11245a5a281f85f6d1c24fa38ffe094d7f5
JuBinLuB <alexssander884@gmail.com>
2023-03-11 11:07:24 -0300

    new file:   Lista 5/Exercicio_10/src/components/ReadOperation.java 	modified:   Lista 5/Exercicio_10/src/test/CalculatorApp.java

---
+++ Lista 5/Exercicio_10/src/components/ReadOperation.java
@@ -0,0 +1,137 @@
+package components;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ReadOperation {
+
+    static Scanner scan = new Scanner(System.in);
+
+    private static boolean validate;
+    private static boolean open;
+    private static int option;
+
+    public static void operationsPanel() {
+
+        ReadOperation.open = true;
+
+        // do {
+
+        System.out.println("-------- Operações --------");
+        System.out.println("[1] - Divisão");
+        System.out.println("[2] - Logaritmo");
+        System.out.println("[3] - Encerrar");
+        System.out.println();
+
+        ReadOperation.option = reading();
+        ReadOperation.validate = false;
+
+        while (!ReadOperation.validate) {
+            System.out.println();
+
+            try {
+
+                switch (ReadOperation.option) {
+
+                    case 1:
+                        System.out.println("-------- Divisão --------");
+                        // ReadOperation.validate = false;
+
+                        // do {
+
+                        // try {
+
+                        System.out.print("Numerador: ");
+                        int p = scan.nextInt();
+                        System.out.print("Denominador: ");
+                        int q = scan.nextInt();
+
+                        double r = Calculator.div(p, q);
+                        System.out.println("Resultado: " + r);
+
+                        ReadOperation.validate = true;
+
+                        // } catch (InvalidOperationException | InputMismatchException e) {
+                        // System.out.println("\nErro: " + e.getMessage());
+                        // e.printStackTrace();
+                        // scan.nextLine();
+                        // }
+
+                        // System.out.println();
+                        // } while (!ReadOperation.validate);
+                        break;
+
+                    case 2:
+                        System.out.println("-------- Logaritmo --------");
+                        // ReadOperation.validate = false;
+
+                        // do {
+
+                        // try {
+
+                        System.out.print("Logaritmando: ");
+                        int log = scan.nextInt();
+
+                        double s = Calculator.log10(log);
+                        System.out.println("Resultado: " + s);
+
+                        ReadOperation.validate = true;
+
+                        // } catch (InvalidOperationException | InputMismatchException e) {
+                        // System.out.println("\nErro: " + e.getMessage());
+                        // e.printStackTrace();
+                        // scan.nextLine();
+                        // }
+
+                        // System.out.println();
+                        // } while (!ReadOperation.validate);
+                        break;
+
+                    case 3:
+                        System.out.print("-------- Calculadora encerrada --------");
+                        ReadOperation.validate = true;
+                        ReadOperation.open = false;
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida.");
+                        break;
+                }
+
+            } catch (InvalidOperationException | InputMismatchException e) {
+                System.out.println("\nErro: " + e.getMessage());
+                e.printStackTrace();
+                scan.nextLine();
+            }
+        }
+
+        System.out.println();
+        // } while (ReadOperation.open);
+
+        if (ReadOperation.open) {
+            ReadOperation.operationsPanel();
+        }
+    }
+
+    public static int reading() {
+
+        ReadOperation.validate = false;
+        int choice = 0;
+
+        do {
+
+            System.out.print("Escolha a operação: ");
+
+            try {
+                choice = scan.nextInt();
+                ReadOperation.validate = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Erro. Insira apenas números inteiros.");
+                scan.nextLine();
+            }
+
+        } while (!ReadOperation.validate);
+
+        return choice;
+    }
+}
--- Lista 5/Exercicio_10/src/test/CalculatorApp.java
+++ Lista 5/Exercicio_10/src/test/CalculatorApp.java
@@ -1,7 +1,10 @@
 package test;
 
+import components.ReadOperation;
+
 public class CalculatorApp {
     public static void main(String[] args) {
 
+        ReadOperation.operationsPanel();
     }
 }
