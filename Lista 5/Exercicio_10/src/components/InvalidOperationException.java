22455841dd21c27b4cc261c25f9e0a505987777e
JuBinLuB <alexssander884@gmail.com>
2023-03-11 11:46:20 -0300

    modified:   Lista 5/Exercicio_10/src/components/InvalidOperationException.java

--- Lista 5/Exercicio_10/src/components/InvalidOperationException.java
+++ Lista 5/Exercicio_10/src/components/InvalidOperationException.java
@@ -3,7 +3,7 @@ package components;
 // Letra E
 // i) Fazendo com que a classe InvalidOperationException herde de RuntimeException;
 // ii) E removendo o tratamento de erro do aplicativo;
-// iii) Ao surgir operações inválidas o programa será imediatamente encerrado pela JVM.
+// iii) Ao surgir operações inválidas, o programa será imediatamente encerrado pela JVM.
 
 public class InvalidOperationException extends Exception {
 
